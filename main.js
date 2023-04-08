255d3bcbb4f3e5a56e66fbd234febca6612f6b0e
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-04-08 13:54:15 +0200

    Weather-App project added

--- main.js
+++ main.js
@@ -348,6 +348,49 @@ const renderProjects = () =>{
               </li>
               <li class="project-element">
                 <div class="project-element-content">
+                  <p class="project-status">Desplegado ✅</p>
+                  <h3 class="project-title">
+                    <a href="https://weather-app-alejandro-luzuriaga.netlify.app/">Weather APP</a>
+                  </h3>
+                  <div class="project-description">
+                    <p>
+                      Aplicación del clima.
+                    </p>
+                  </div>
+                  <ul class="project-technologies">
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                  </ul>
+                  <div class="project-links">
+                    <a href="https://github.com/alejandroluzuriaga/Weather-App">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github"
+                        width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ccd6f6" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path
+                          d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                      </svg>
+                    </a>
+                    <a href="https://weather-app-alejandro-luzuriaga.netlify.app/">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ccd6f6" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                        <line x1="10" y1="14" x2="20" y2="4" />
+                        <polyline points="15 4 20 4 20 9" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <div class="project-element-image">
+                  <div class="project-element-image-container">
+                    <img src="https://res.cloudinary.com/djgkryxou/image/upload/v1680954785/Portfolio/WeatherApp_fm9vl5.png" alt="Weather-App-photo">
+                  </div>
+                </div>
+              </li>
+              <li class="project-element">
+                <div class="project-element-content">
                   <p class="project-status"> Muy pronto 🚀</p>
                   <h3 class="project-title">
                     <a href="#">API Rest</a>
