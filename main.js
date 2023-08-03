902cc6688cf63130f51fa9cf84cf35606a52a4d3
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-08-03 16:54:24 +0200

    Corregido error en elemento 'Tienda dinamica' en proyectos (enlace erroneo)

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ const renderProjects = () =>{
                           d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                       </svg>
                     </a>
-                    <a href="https://portfolio-alejandro-luzuriaga.netlify.app/">
+                    <a href="https://tienda-dinamica-alejandroluzuriaga.netlify.app/">
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ccd6f6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
