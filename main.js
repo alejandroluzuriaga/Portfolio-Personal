776301e51f0d644e7fae6c9e4d37945616004de5
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-08-07 00:26:56 +0200

    Proyecto API REST RELACIONAL añadido

--- main.js
+++ main.js
@@ -465,6 +465,38 @@ const renderProjects = () =>{
                   </div>
                 </div>
               </li>
+              <li class="project-element">
+                <div class="project-element-content">
+                  <p class="project-status"> Desplegado ✅</p>
+                  <h3 class="project-title">
+                    <a href="https://github.com/alejandroluzuriaga/API-REST">API Rest Relacional</a>
+                  </h3>
+                  <div class="project-description">
+                  </div>
+                  <ul class="project-technologies">
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MongoDB</li>
+                    <li>Mongoose</li>
+                  </ul>
+                  <div class="project-links">
+                    <a href="https://github.com/alejandroluzuriaga/API-REST-RELACIONAL">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github"
+                        width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ccd6f6" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path
+                          d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <div class="project-element-image">
+                  <div class="project-element-image-container">
+                    <img src="https://res.cloudinary.com/djgkryxou/image/upload/v1691360763/Portfolio/API-RELACIONAL_dsayhu.png" alt="API REST RELACIONAL">
+                  </div>
+                </div>
+              </li>
             </ul>
           </section>
   `
